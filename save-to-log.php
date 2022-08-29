a0521e83e2351cbdc1afe49e7ae58e372a97122a
kiAVASHWorld <83844951+kiAVASHWorld@users.noreply.github.com>
2022-08-29 17:52:36 +0430

    Update save-to-log.php

--- save-to-log.php
+++ save-to-log.php
@@ -1,11 +1,5 @@
 <?php
-  $to_email = 'kiavashworld@gmail.com';
-  $subject = 'User feedback from site';
-  $message = $_POST['message'];
-
-  // this may need configuring depending on your host. If you find the email isn't
-  // being sent, look up the error you're receiving or post another question here on
-  // SO.
-  mail($to_email, $subject, $message);
-
-  header('Location: /');
+  $log_file_name = 'mylog.log'; // Change to the log file name
+  $message = $_POST['message']; // incoming message
+  file_put_contents($log_file_name, $message, FILE_APPEND);
+  header('Location: /'); // redirect back to the main site
