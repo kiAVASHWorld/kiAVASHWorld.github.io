01b90d603462605aec9a73d4bee430167308ec71
kiAVASHWorld <83844951+kiAVASHWorld@users.noreply.github.com>
2022-08-29 17:59:33 +0430

    Update save-to-log.php

--- save-to-log.php
+++ save-to-log.php
@@ -1,21 +1,2 @@
 <?php
-$to = "xyz@somedomain.com";
-$subject = "This is subject";
-  
-$message = $_POST['message'];
-$message .= "<h1>This is headline.</h1>";
-  
-$header = "From:abc@somedomain.com \r\n";
-$header .= "Cc:afgh@somedomain.com \r\n";
-$header .= "MIME-Version: 1.0\r\n";
-$header .= "Content-type: text/html\r\n";
-  
-$retval = mail ($to,$subject,$message,$header);
-  
-if( $retval == true ) {
-     echo "Message sent successfully...";
-}else {
-     echo "Message could not be sent...";
-}
-//echo "<h2>"+$_POST['message']+"</h2>";
-?>
+echo "<h2>"+$_POST['message']+"</h2>";
