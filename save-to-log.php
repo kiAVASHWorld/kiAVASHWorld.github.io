c4610016b0b27cc76bc1a8ac39ca66743dcb7751
kiAVASHWorld <83844951+kiAVASHWorld@users.noreply.github.com>
2022-08-29 18:12:42 +0430

    Update save-to-log.php

--- save-to-log.php
+++ save-to-log.php
@@ -1,3 +1,14 @@
-<?php
-echo "<h2>"+$_POST['message']+"</h2>";
-?>
+<html>
+   
+   <head>
+      <title>Sending HTML email using PHP</title>
+   </head>
+   
+   <body>
+      
+   <?php
+   echo "<h2>"+$_POST['message']+"</h2>";
+   ?>
+      
+   </body>
+</html>
