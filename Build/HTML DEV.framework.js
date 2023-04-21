198733096b83d2b105849dd45b86698e21503b53
kiAVASHWorld <83844951+kiAVASHWorld@users.noreply.github.com>
2023-04-21 13:57:46 +0430

    .

--- Build/HTML DEV.framework.js
+++ Build/HTML DEV.framework.js
@@ -1297,7 +1297,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2510032;
+STATICTOP = STATIC_BASE + 2509760;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3283,7 +3283,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2510032;
+var STATIC_BUMP = 2509760;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
